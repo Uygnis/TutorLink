44e703c9d49ee89e3db8c3e7f7af1dbc6085a669
chowsowying <sow99dev@gmail.com>
2025-08-13 20:35:50 +0800

    Attempt to fix pipeline build failure

--- backend/src/test/java/com/csy/springbootauthbe/SpringbootAuthBeApplicationTests.java
+++ backend/src/test/java/com/csy/springbootauthbe/SpringbootAuthBeApplicationTests.java
@@ -1,9 +1,11 @@
 package com.csy.springbootauthbe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SpringbootAuthBeApplicationTests {
 
 	@Test
