34c893cc24e6edf73e7e2b9135fea71f47a41745
saltedfishxx <saltedfishxx@gmail.com>
2025-08-14 16:09:48 +0800

    removed filter method

--- backend/src/main/java/com/csy/springbootauthbe/config/JWTAuthenticationFilter.java
+++ backend/src/main/java/com/csy/springbootauthbe/config/JWTAuthenticationFilter.java
@@ -21,13 +21,6 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     private final JWTService jwtService;
     private final UserDetailsService userDetailsService;
-
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        String path = request.getRequestURI();
-        // Skip JWT check for authentication endpoints
-        return path.startsWith("/api/v1/auth");
-    }
     
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
