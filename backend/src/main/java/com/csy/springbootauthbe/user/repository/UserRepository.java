e1bcdf93da3a1a784e9869e9f1574aa3511acbca
Uygnis <vesperlsy@gmail.com>
2025-10-30 16:26:57 +0800

    allow creation if email exist but status deleted

--- backend/src/main/java/com/csy/springbootauthbe/user/repository/UserRepository.java
+++ backend/src/main/java/com/csy/springbootauthbe/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.csy.springbootauthbe.user.repository;
 
+import com.csy.springbootauthbe.user.entity.AccountStatus;
 import com.csy.springbootauthbe.user.entity.Role;
 import com.csy.springbootauthbe.user.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,7 +17,11 @@ public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByEmailAndStatusNot(String email, AccountStatus status);
+
     boolean existsByEmail(String email);
+
+    boolean existsByEmailAndStatusNot(String email, AccountStatus status);
 
     List<User> findAllByRole(Role role);
 
